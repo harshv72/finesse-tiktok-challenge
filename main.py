bce58b8b4f22b3a5f8a5d56da949a1c8b29ba33c
Harsh Vaghasiya <52853300+harshv72@users.noreply.github.com>
2023-11-09 15:08:27 -0800

    Implemented Browser automation using Playwright

--- main.py
+++ main.py
@@ -1,19 +1,93 @@
 import json
-
+import random
 import requests
 from bs4 import BeautifulSoup
+from collections import namedtuple
+from urllib.parse import urlencode, quote
+from playwright.sync_api import sync_playwright
 
-# Request object with Session maintained
-session = requests.Session()
+# Browser session state
+session = namedtuple('session', ['browser', 'context', 'page', 'info'])
 
-# Common Headers for Session
+# Default Configs
+configs = {
+    "Lang": "en",
+    "Locale": "en-US",
+    "TimeZone": "America/Chicago",
+    "DefaultURL": "https://www.tiktok.com/@redbull?lang=en",
+    "RndDeviceID": str(random.randint(10 ** 18, 10 ** 19 - 1)),
+    "UserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+}
+
+# Common Headers for APIs
 headers = {
+    "Origin": "https://www.tiktok.com/",
     "Referer": "https://www.tiktok.com/",
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Authority': 'www.tiktok.com',
+    "User-Agent": configs["UserAgent"],
 }
+
+
+def get_params():
+    return {
+        "aid": "1988",
+        "app_language": configs["Lang"],
+        "app_name": "tiktok_web",
+        "browser_language": session.info["browser_language"],
+        "browser_name": "Mozilla",
+        "browser_online": "true",
+        "browser_platform": session.info["browser_platform"],
+        "browser_version": session.info["user_agent"],
+        "channel": "tiktok_web",
+        "cookie_enabled": "true",
+        "device_id": configs["RndDeviceID"],
+        "device_platform": "web_pc",
+        "focus_state": "true",
+        "from_page": "user",
+        "history_len": session.info["history"],
+        "is_fullscreen": "false",
+        "is_page_visible": "true",
+        "language": configs["Lang"],
+        "os": session.info["platform"],
+        "priority_region": "",
+        "referer": "",
+        "region": "US",
+        "screen_height": session.info["screen_height"],
+        "screen_width": session.info["screen_width"],
+        "tz_name": configs["TimeZone"],
+        "webcast_language": configs["Lang"],
+    }
+
+
+def fetch_data(url, headers):
+    headers_js = json.dumps(headers)
+    js_fetch = f"""
+        () => {{
+            return new Promise((resolve, reject) => {{
+                fetch('{url}', {{ method: 'GET', headers: {headers_js} }})
+                    .then(response => response.json())
+                    .then(data => resolve(data))
+                    .catch(error => reject(error.message));
+            }});
+        }}
+    """
+    return session.page.evaluate(js_fetch)
+
+
+def encode_url(base, params):
+    return f"{base}?{urlencode(params, quote_via=quote)}"
+
+
+# Close current browser instance
+def session_close():
+    if session.browser is not None:
+        session.browser.close()
+    exit(0)
 
 
 def extract_stateinfo(content):
@@ -21,34 +95,19 @@ def extract_stateinfo(content):
     hydra_data = soup.find_all('script', {'id': '__UNIVERSAL_DATA_FOR_REHYDRATION__'})
     if len(hydra_data) > 0:
         js_state = hydra_data[0]
-        unescaped = js_state.text.encode('utf-8').decode('unicode_escape')
+        # js_state.text.encode('utf-8').decode('unicode_escape'), Corrupts the non-Ascii characters
+        unescaped = js_state.text.replace("//", "/")
         return json.loads(unescaped)
     return None
 
 
 def fetch_recommenations(count=10):
-    url = ("https://www.tiktok.com/api/recommend/item_list/?aid=1988&app_language=en&app_name=tiktok_web"
-           "&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=MacIntel"
-           "&browser_version=5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%2016_6%20like%20Mac%20OS%20X%29%20AppleWebKit%2F60"
-           "5.1.15%20%28KHTML%2C%20like%20Gecko%29%20Version%2F16.6%20Mobile%2F15E148%20Safari%2F604.1"
-           "&channel=tiktok_web&cookie_enabled=true&count=9&coverFormat=0&device_id=7300197118059234859"
-           "&device_platform=web_mobile&focus_state=true&from_page=fyp&history_len=6&isNonPersonalized=false"
-           "&is_fullscreen=false&is_page_visible=true&os=ios&priority_region=&pullType=1&referer=&region=US"
-           "&screen_height=932&screen_width=430&tz_name=Asia%2FCalcutta&webcast_language=en"
-           "&msToken=lZLoLEEK53Wl91zNeiFDUJ2k9O354VG_svE3qtWERQqSNCgMtk0jIFM2JZvLm_vV8m_VKYmnWx-GnN9unfzicMnJ3"
-           "ApvGB_c6UCmxj5piLFy4aDDVHtUhcb3UDqeCilPMANkt5dx9eRGTrnp2rI="
-           "&X-Bogus=DFSzsIVuTEiANn7BtFznfU9WcBJz"
-           "&_signature=_02B4Z6wo00001O1TKPwAAIDA7VMo.941d5ztUSxAAF4h50")
+    base_url = "https://www.tiktok.com/api/recommend/item_list/"
 
-    # r = session.get(url, headers=headers)
-    # if r.status_code != 200:
-    #     return None
-
-    # print(r.content)
-
-    # result = r.json()
-    # if 'itemList' not in result:
-    #     return None
+    params = get_params()
+    params["from_page"] = "fyp"
+    params["count"] = count
+    params["coverFormat"] = "0"
 
     res = []
     first = True
@@ -59,11 +118,9 @@ def fetch_recommenations(count=10):
         realCount = 100
 
     while len(res) < count:
-        r = session.get(url.format(realCount), headers=headers)
-        if r.status_code != 200:
-            break
+        params["count"] = realCount
+        result = fetch_data(encode_url(base_url, params), headers)
 
-        result = r.json()
         if 'itemList' not in result:
             break
 
@@ -80,42 +137,88 @@ def fetch_recommenations(count=10):
     return res[:count]
 
 
-def get_user_info(hashtag="redbull"):
-    url = "https://www.tiktok.com/@{}".format(hashtag)
+def fetch_challenge(cid=11885, count=30):
+    base_url = "https://www.tiktok.com/api/challenge/item_list/"
 
-    r = session.get(url, headers=headers)
+    params = get_params()
+    params["challengeID"] = cid
+    params["count"] = count
+    params["coverFormat"] = "2"
+    params["cursor"] = "0"
+
+    return fetch_data(encode_url(base_url, params), headers)
+
+
+def get_user_info(hashtag="redbull"):
+    user_url = "https://www.tiktok.com/@{}".format(hashtag)
+
+    r = requests.get(user_url, headers=headers)
     if r.status_code != 200:
         return None
 
-    return extract_stateinfo(r.content)
+    data = extract_stateinfo(r.content)
+    if "__DEFAULT_SCOPE__" in data:
+        data = data["__DEFAULT_SCOPE__"]
+        if "webapp.user-detail" in data:
+            return data["webapp.user-detail"]
+    return None
 
 
 if __name__ == '__main__':
     print('[=>] TikTok Fashion Scraper Starting')
 
-    # Starts with origin url to load important cookies
-    r = session.get("https://m.tiktok.com/", headers=headers, allow_redirects=True)
-    if r.status_code != 200:
-        print("[!] Failed to Connect to Origin")
-        exit(0)
-    print(r.cookies)
+    with sync_playwright() as playwright:
+        mobile_device = playwright.devices['iPhone 14 Pro Max']
 
-    # recomd = fetch_recommenations(10)
-    # if not recomd:
-    #     print("[!] Failed to get to Recommended Posts")
-    #     exit(0)
+        session.browser = playwright.chromium.launch(
+            headless=True,
+            args=["--user-agent={}".format(configs["UserAgent"])],
+            ignore_default_args=["--mute-audio", "--hide-scrollbars"],
+        )
 
-    # count = 0
-    # for rec in recomd:
-    #     print(f'[=>] Post {count + 1}: {rec["id"]}')
-    #     print(f'[*] Description: {rec["desc"]}')
-    #     print(f'[*] Play Count: {rec["stats"]["playCount"]}')
-    #     print(f'[*] Share Count: {rec["stats"]["shareCount"]}')
-    #     print(f'[*] Comment Count: {rec["stats"]["commentCount"]}')
-    #     print(f'[*] Author: {rec["author"]["nickname"]}')
-    #     print()
-    #     count += 1
+        session.context = session.browser.new_context(
+            **mobile_device,
+            bypass_csp=True,
+            locale=configs["Locale"],
+            timezone_id=configs["TimeZone"],
+        )
 
-    get_user_info("redbull")
-    
+        session.page = session.context.new_page()
+        session.page.goto(configs["DefaultURL"], wait_until="networkidle")
+        session.info = session.page.evaluate("""() => {
+          return {
+            platform: window.navigator.platform,
+            deviceScaleFactor: window.devicePixelRatio,
+            user_agent: window.navigator.userAgent,
+            screen_width: window.screen.width,
+            screen_height: window.screen.height,
+            history: window.history.length,
+            browser_language: window.navigator.language,
+            browser_platform: window.navigator.platform,
+            browser_name: window.navigator.appCodeName,
+            browser_version: window.navigator.appVersion,
+          };
+        }""")
+
+        # result = fetch_challenge()
+        # print(result)
+
+        recomd = fetch_recommenations(10)
+        if not recomd:
+            print("[!] Failed to get to Recommended Posts")
+            session_close()
+
+        count = 0
+        for rec in recomd:
+            print(f'[=>] Post {count + 1}: {rec["id"]}')
+            print(f'[*] Description: {rec["desc"]}')
+            print(f'[*] Play Count: {rec["stats"]["playCount"]}')
+            print(f'[*] Share Count: {rec["stats"]["shareCount"]}')
+            print(f'[*] Comment Count: {rec["stats"]["commentCount"]}')
+            print(f'[*] Author: {rec["author"]["nickname"]}')
+            print()
+            count += 1
+
+        session.browser.close()
+
     print("[=>] TikTok Fashion Scraper Stopped")
